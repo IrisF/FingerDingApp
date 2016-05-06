7f4e9626670d149304884e972d680ce18e506bfd
pranges <pranges@cip.ifi.lmu.de>
2016-05-06 18:14:58 +0200

    first steps with zooming gesture

--- studio/Medieninformatik/app/src/main/java/ifi/lmu/com/handmeasurementstudy/Zooming.java
+++ studio/Medieninformatik/app/src/main/java/ifi/lmu/com/handmeasurementstudy/Zooming.java
@@ -2,16 +2,53 @@ package ifi.lmu.com.handmeasurementstudy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 
 
 public class Zooming extends ActionBarActivity {
+
+    private ScaleGestureDetector scaleGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zooming);
+
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.OnScaleGestureListener() {
+            @Override
+            public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+                Log.e("e", "On Scale");
+                return false;
+            }
+
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+                Log.e("Scale", "############################### new Scaling Event #####################################");
+                Log.e("Scale", "Current Span " + Float.toString(scaleGestureDetector.getCurrentSpan()));
+                Log.e("Scale", "Current Coord X " + Float.toString(scaleGestureDetector.getCurrentSpanX()));
+                Log.e("Scale", "Current Coord Y " + Float.toString(scaleGestureDetector.getCurrentSpanY()));
+                Log.e("Scale", "Focus X " + Float.toString(scaleGestureDetector.getFocusX()));
+                Log.e("Scale", "Focus Y " + Float.toString(scaleGestureDetector.getFocusY()));
+                Log.e("Scale", "Scale Factor " + Float.toString(scaleGestureDetector.getScaleFactor()));
+                Log.e("Scale", "Time Delta " + Float.toString(scaleGestureDetector.getTimeDelta()));
+                Log.e("Scale", "Event Time " + Long.toString(scaleGestureDetector.getEventTime()));
+                return false;
+            }
+
+            @Override
+            public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+                Log.e("e", "On Scale End");
+            }
+        });
+    }
+
+    public boolean onTouchEvent(MotionEvent ev) {
+        scaleGestureDetector.onTouchEvent(ev);
+        return true;
     }
 
     @Override
